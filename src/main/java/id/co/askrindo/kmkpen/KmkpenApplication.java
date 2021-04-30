e93c02a16229d712c6f4eace72fcfc00027e6ec7
Anggoro Biandono <83442434+anggoro-askrindo@users.noreply.github.com>
2021-04-30 11:46:16 +0700

    Update KmkpenApplication.java
    Tes Edit dari browser

--- src/main/java/id/co/askrindo/kmkpen/KmkpenApplication.java
+++ src/main/java/id/co/askrindo/kmkpen/KmkpenApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KmkpenApplication {
 
 	public static void main(String[] args) {
-		System.out.println("##Test Commit");
 		SpringApplication.run(KmkpenApplication.class, args);
 	}
 
